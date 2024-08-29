ebd7202b854bfe8cc2d3c659e3b9933ef135da11
HyeIn <helen6662@naver.com>
2024-08-29 12:19:09 +0900

    [feat] 객체

--- Section2/src/chapter3.ts
+++ Section2/src/chapter3.ts
@@ -1,18 +1,40 @@
-let user: {
+// 특수한 프로퍼티
+let user1: {
     id: number;
     name: string;
 } = {
     id: 1,
-    name: "양혜인",
+    name: "가나다",
 };
 
-user.id;
-
-
-let dog: {
+//선택적 프로퍼티(Optional Property)
+let user2: {
+    id: number;
     name: string;
-    color: string;
 } = {
-    name: "돌돌이",
-    color: "brown",
+    id: 2,
+    name: "라마바",
 };
+
+// user2 = { // x 오류 발생
+//     name: "홍길동",
+// };
+
+let user3: {
+    id?: number;
+    name: string;
+} = {
+    id: 3,
+    name: "사아자",
+};
+
+// 읽기전용 프로퍼티
+let user4: {
+    id?: number;
+    readonly name: string;
+} = {
+    id: 4,
+    name: "차카타",
+};
+
+// user4.name = "dskfd"; // x 오류 발생
