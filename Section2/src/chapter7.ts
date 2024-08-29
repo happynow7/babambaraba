e9233d2ec26a7e7af8ba70bd5e30162e1af2e305
HyeIn <helen6662@naver.com>
2024-08-29 12:32:02 +0900

    [feat] void와 never

---
+++ Section2/src/chapter7.ts
@@ -0,0 +1,27 @@
+// void
+function func1(): string {
+    return "hello";
+}
+
+function func2(): void {
+    console.log("hello");
+}
+
+// never
+function func3(): never {
+    while (true) {}
+}
+
+function func4(): never {
+    throw new Error();
+}
+
+let anyVar: any;
+
+let a: never;
+// a = 1;
+// a = {};
+// a = "";
+// a = undefined;
+// a = null;
+// a = anyVar;
