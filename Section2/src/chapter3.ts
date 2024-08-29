f7f69f2a6307e4d5b580335e2c9e212fad039846
HyeIn <helen6662@naver.com>
2024-08-29 12:15:57 +0900

    [feat] 객체리터럴

---
+++ Section2/src/chapter3.ts
@@ -0,0 +1,18 @@
+let user: {
+    id: number;
+    name: string;
+} = {
+    id: 1,
+    name: "양혜인",
+};
+
+user.id;
+
+
+let dog: {
+    name: string;
+    color: string;
+} = {
+    name: "돌돌이",
+    color: "brown",
+};
